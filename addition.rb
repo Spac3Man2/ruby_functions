261d8e18dfb5477925516372b11ced8c0f624845
Spac3Man2 <Spac3Man2>
2017-01-25 12:14:12 -0500

    new file

--- addition.rb
+++ addition.rb
@@ -1,3 +1,5 @@
 def add(term1, term2)
     return term1 + term2 
-end    
+end   
+
+
--- test_addition.rb
+++ test_addition.rb
@@ -6,6 +6,21 @@ class Test_addition <Minitest::Test
   def test_1_plus_1_returns_2
 	assert_equal(2, add(1, 1))
   end
+  
+  def test_37_plus_52_retruns_89
+  	assert_equal(89, add(37, 52))
+  end
 
-end  
-      
+  def test_65_plus_70_returns_135
+  	assert_equal(135, add(65, 70))
+  end
+
+  def test_115_plus_90_returns_205
+  	assert_equal(205, add(115, 90))
+  end
+# 3_0 is 3.0, 6_9 is 6.9
+  def test_3_0_plus_6_9_returns_9_9
+  	assert_equal(9.9, add(3.0, 6.9))
+  end
+   	
+end      
